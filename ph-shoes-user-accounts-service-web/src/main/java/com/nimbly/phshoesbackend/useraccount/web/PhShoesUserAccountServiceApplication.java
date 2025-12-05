d408ea3e4fa63ce05223be9cba5b94621ed5ff33
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-12-05 18:30:26 +0800

    Fix

--- ph-shoes-user-accounts-service-web/src/main/java/com/nimbly/phshoesbackend/useraccount/web/PhShoesUserAccountServiceApplication.java
+++ ph-shoes-user-accounts-service-web/src/main/java/com/nimbly/phshoesbackend/useraccount/web/PhShoesUserAccountServiceApplication.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 		"com.nimbly.phshoesbackend.useraccount.web",
 		"com.nimbly.phshoesbackend.notification.core",
 		"com.nimbly.phshoesbackend.notification.email.providers",
-		"com.nimbly.phshoesbackend.services.common.core"
+		"com.nimbly.phshoesbackend.commons.core",
+		"com.nimbly.phshoesbackend.commons.web"
 })
 public class PhShoesUserAccountServiceApplication {
 	public static void main(String[] args) {
